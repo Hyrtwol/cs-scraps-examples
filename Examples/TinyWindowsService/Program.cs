5284332b992e372869c235573eaf105dea1f3e24
Thomas la Cour <tlc@247e.com>
2015-05-06 15:25:44 +0200

    windows service example

---
+++ Examples/TinyWindowsService/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using Scraps;
+
+namespace TinyWindowsService
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new TinyService();
+            WindowsService.Run(service);
+        }
+    }
+
+    internal class TinyService : IWindowsService
+    {
+        private static readonly ManualResetEvent TerminateEvent = new ManualResetEvent(false);
+        private Thread _thread;
+
+        public TinyService()
+        {
+            _thread = null;
+        }
+
+        public void Dispose()
+        {
+            if (_thread != null) Stop();
+        }
+
+        public string Name { get { return GetType().Name; } }
+
+        public void Start()
+        {
+            if (_thread != null) throw new InvalidOperationException("Service already started");
+            _thread = new Thread(DoWork);
+            _thread.Name = Name + "Thread";
+            _thread.Start("some value");
+        }
+
+        public void Stop()
+        {
+            if (_thread == null) return;
+            TerminateEvent.Set();
+            if (!_thread.Join(TimeSpan.FromSeconds(10)))
+            {
+                _thread.Abort();
+                _thread.Join();
+            }
+            _thread = null;
+        }
+
+        private static void DoWork(object obj)
+        {
+            Console.WriteLine(obj);
+            var randomizer = new Random(DateTime.Now.Millisecond);
+
+            while (!TerminateEvent.WaitOne(0))
+            {
+                //  Get values that will fool the boss
+                int zipcode = randomizer.Next(0, 20000);
+                int temperature = randomizer.Next(-80, 135);
+                int relativeHumidity = randomizer.Next(10, 60);
+
+                string update = string.Format("{0} {1} {2}", zipcode, temperature, relativeHumidity);
+
+                Console.WriteLine(update);
+            }
+        }
+    }
+}
---
+++ Scraps/WindowsService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace Scraps
+{
+    public interface IWindowsService : IDisposable
+    {
+        string Name { get; }
+        void Start();
+        void Stop();
+    }
+
+    public class WindowsService : ServiceBase
+    {
+        // Required designer variable.
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        private System.ComponentModel.IContainer components = null;
+
+        // http://msdn.microsoft.com/en-us/library/system.serviceprocess.serviceinstaller(v=vs.110).aspx
+
+        private const int DefaultConsoleWidth = 120;
+        private const int DefaultConsoleHeight = 25;
+        private static readonly string Title = Assembly.GetEntryAssembly().GetName().Name;
+        private static readonly ConsoleColor DefaultColor = Console.ForegroundColor;
+
+        public static void Run(IWindowsService service)
+        {
+            var texlService = new WindowsService(service);
+            if (Environment.UserInteractive)
+            {
+                ConfigureConsole();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("   .oOo.   .oOo.   .oOo.    Starting {0}     .oOo.   .oOo.   .oOo.   ", service.Name);
+                Console.ForegroundColor = DefaultColor;
+                texlService.OnStart(null);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("   .oOo.   .oOo.   .oOo.    Press ESC to stop    .oOo.   .oOo.   .oOo.   ");
+                Console.ForegroundColor = DefaultColor;
+                WairForEscape();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("   .oOo.   .oOo.   .oOo.    Stopping {0}     .oOo.   .oOo.   .oOo.   ", service.Name);
+                Console.ForegroundColor = DefaultColor;
+                texlService.Stop();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("   .oOo.   .oOo.   .oOo.    Press ESC to exit    .oOo.   .oOo.   .oOo.   ");
+                Console.ForegroundColor = DefaultColor;
+                WairForEscape();
+            }
+            else
+            {
+                Run(texlService);
+            }
+        }
+
+        private readonly IWindowsService _service;
+
+        private WindowsService(IWindowsService service)
+        {
+            ServiceName = service.Name;
+            _service = service;
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            if (_service != null) _service.Dispose();
+            base.Dispose(disposing);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _service.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _service.Stop();
+        }
+
+        private static void WairForEscape()
+        {
+            ConsoleKey key;
+            do
+            {
+                if (Console.KeyAvailable) key = Console.ReadKey(true).Key;
+                else
+                {
+                    key = 0;
+                    Thread.Sleep(200);
+                }
+            } while (key != ConsoleKey.Escape);
+        }
+
+        private static void ConfigureConsole()
+        {
+            Console.Title = Title;
+            Console.SetWindowSize(DefaultConsoleWidth, DefaultConsoleHeight);
+            Console.SetBufferSize(DefaultConsoleWidth, 100);
+
+            var windowPosition = ConfigurationManager.AppSettings["window-position"];
+            if (!string.IsNullOrEmpty(windowPosition))
+            {
+                var vals = windowPosition.Split(' ').Select(int.Parse).ToArray();
+                if (vals.Length == 2) ConsoleUtils.SetWindowPos(vals[0], vals[1]);
+            }
+        }
+
+        private static class ConsoleUtils
+        {
+            private static readonly IntPtr ThisConsole = GetConsoleWindow();
+
+            public static void SetWindowPos(int left, int top)
+            {
+                SetWindowPos(ThisConsole, 0, left, top, 0, 0, 0x0001);
+            }
+
+            [DllImport("kernel32.dll", ExactSpelling = true)]
+            private static extern IntPtr GetConsoleWindow();
+
+            [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+            private static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+        }
+    }
+}
